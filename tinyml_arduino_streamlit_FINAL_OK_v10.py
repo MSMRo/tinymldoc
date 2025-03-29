7fa3e8c730a100cbafbd6a0011f52e7358074d9b
Moises Meza <57838764+MSMRo@users.noreply.github.com>
2025-03-29 19:16:06 +0000

    add link libraries

--- tinyml_arduino_streamlit_FINAL_OK_v10.py
+++ tinyml_arduino_streamlit_FINAL_OK_v10.py
@@ -40,6 +40,10 @@ st.header(section)
 if section == "Introducción":
     st.markdown("""TensorFlow Lite Micro para Arduino""")
     st.markdown("""La biblioteca TensorFlow Lite Micro Library for Arduino permite ejecutar modelos de aprendizaje automático en microcontroladores compatibles con Arduino. Proporciona ejemplos y código necesario para integrar modelos de TensorFlow Lite en proyectos de Arduino.
+
+Las Librerias de los sensores se encuentra en: [https://docs.arduino.cc/hardware/nano-33-ble-sense/#suggested-libraries](https://docs.arduino.cc/hardware/nano-33-ble-sense/#suggested-libraries)
+
+
 ### Tabla de Contenidos
 - Estructura de la Biblioteca
 - Clases y Métodos Principales
